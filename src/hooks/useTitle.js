2d27c484436a533cb7eff24664a7f6b855be81e0
ninad-debug <ninadsamant07@gmail.com>
2024-01-14 10:34:54 +0530

    title change in Hooks

--- src/hooks/useTitle.js
+++ src/hooks/useTitle.js
@@ -1,9 +1,9 @@
 import { useEffect } from "react";
 
 export const useTitle = (title) => {
-    useEffect(()=>{
-        document.title = `${title} / Cinemate`
-    });
+  useEffect(() => {
+    document.title = `${title} / MovieDb`;
+  });
 
   return null;
-}
+};
